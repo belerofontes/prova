6c35d7fc22bf7d1b4adcde5c4bdcc8e44a10b0a6
paulo.rogerio.oli@hotmail.com <07225737660@B06M14.pedagogico.local>
2015-07-02 21:11:03 -0300

    comentt

--- importacao/src/br/edu/iftm/importacao/taxas.java
+++ importacao/src/br/edu/iftm/importacao/taxas.java
@@ -1,5 +1,37 @@
 package br.edu.iftm.importacao;
 
 public class taxas {
+	
+	private String produto = "";
+	private String fiscal= "";
+	private double imposto = 20;
+	
+	public String getProduto() {
+		return produto;
+	}
+	public void setProduto(String produto) {
+		if(produto !=null)
+		{
+		this.produto = produto;
+		}
+	}
+	public String getFiscal() {
+		return fiscal;
+	}
+	public void setFiscal(String fiscal) {
+		this.fiscal = fiscal;
+	}
+	public double getImposto() {
+		return imposto;
+	}
+	
+	public void setImposto(double imposto) {
+		if (imposto >=20 && imposto <=120)
+		{	
+		this.imposto = imposto;
+	//*nada a mais	
+		}
+	}
 
 }
+
